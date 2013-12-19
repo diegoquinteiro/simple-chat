1c1ec08a7cce01b5d1bc0221c8bfd2eeb7079d5e
Diego Moreno Quinteiro <diegoquinteiro@gmail.com>
2013-12-19 02:02:37 -0200

    Create rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleChat::Application.config.secret_key_base = '9622a74393ef2a5eab25951ba7997a5fbef53ea00b5af2f6d3e3808b4c4960c8e430449daff92a89baa3782f7e2153c421a41a4c2ac7c7ddc36b79439c421bc1'
